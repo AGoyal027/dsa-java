e04b17626ee73b2224b3298ad905e7eeb812e70f
AGoyal027 <ayushgoyal027@gmail.com>
2026-05-12 23:55:01 +0530

    Arrays: Print all elements of an array

---
+++ src/main/java/com/goyal/dsa_java/array/PrintArray.java
@@ -0,0 +1,11 @@
+package com.goyal.dsa_java.array;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		int arr[] = { 3, 1, 9, 2, 6, 5 };
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
