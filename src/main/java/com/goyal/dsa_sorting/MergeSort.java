49dbd901105b6e00b2b975340e0c4b787b7eaceb
AGoyal027 <ayushgoyal027@gmail.com>
2026-05-25 23:36:36 +0530

    Sorting II: Merge Sort

---
+++ src/main/java/com/goyal/dsa_sorting/MergeSort.java
@@ -0,0 +1,63 @@
+package com.goyal.dsa_sorting;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MergeSort {
+	
+	static void merge(int[] arr, int low, int mid, int high) {
+		ArrayList<Integer> temp = new ArrayList<>();
+		//[low ... mid]
+		//[mid+1...high]
+		int left = low;
+		int right = mid+1;
+		while(left<=mid && right<=high) {
+			if(arr[left]<=arr[right]) {
+				temp.add(arr[left]);
+				left++;
+			}
+			else {
+				temp.add(arr[right]);
+				right++;
+			}
+		}
+		
+		while(left<=mid) {
+			temp.add(arr[left]);
+			left++;
+		}
+		
+		while(right<=high) {
+			temp.add(arr[right]);
+			right++;
+		}
+		
+		for(int i=low; i<=high; i++) {
+			arr[i] = temp.get(i-low);
+		}
+	}
+	
+	static void mergeSort(int[] nums, int low, int high) {
+		if(low==high) { return; }
+		int mid=(low+high)/2;
+		mergeSort(nums, low, mid);
+		mergeSort(nums, mid+1, high);
+		merge(nums, low, mid, high);
+    }
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for(int i = 0; i<n; i++) {
+				arr[i] = sc.nextInt();
+			}
+			mergeSort(arr, 0, n-1);
+			for (int x : arr) {
+				System.out.print(x + " ");
+			}
+		}
+
+	}
+
+}
